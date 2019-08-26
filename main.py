8d0f06821805463b273e82b895f93abdf0efa47e
Cleyton <cleyton@cleytongoncalves.com>
2019-08-26 14:58:54 -0400

    aa

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from keras import backend as k, callbacks
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as k, callbacks, applications
+from keras.layers import Flatten, Dense, Dropout
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
+from numpy import random
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 start = time.perf_counter()
@@ -21,19 +21,24 @@ IMG_WIDTH, IMG_HEIGHT = 50, 50
 NUM_EPOCHS = 30
 BATCH_SZ = 8
 
-MODEL_SAVE_PATH = f'models/{BATCH_SZ}-{{epoch:02d}} {{val_acc:.2f}}-{{val_loss:.2f}}.hdf5'
+run_id = random.randint(low=1, high=1000)
+MODEL_SAVE_PATH = f'models/({run_id}) {{val_acc:.2f}}-{{val_loss:.2f}} {{epoch:02d}}-{BATCH_SZ}.hdf5'
 
+gpu_options = tf.GPUOptions(allow_growth=True)
 print(f'Using GPU: {k.tensorflow_backend._get_available_gpus()}')
+
 np.random.seed(SEED_RANDOM)
 
-train_datagen = ImageDataGenerator(rescale=1. / 255., validation_split=0.25)
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+train_datagen = ImageDataGenerator(rescale=1. / 255., validation_split=0.25,
+                                   preprocessing_function=applications.densenet.preprocess_input)
+test_datagen = ImageDataGenerator(rescale=1. / 255,
+                                  preprocessing_function=applications.densenet.preprocess_input)
 
 train_generator = train_datagen.flow_from_directory(
     TRAIN_DATA_DIR,
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SZ,
-    color_mode='grayscale',
+    color_mode='rgb',
     seed=SEED_RANDOM,
     shuffle=True,
     class_mode='categorical'
@@ -43,7 +48,7 @@ test_generator = test_datagen.flow_from_directory(
     TEST_DATA_DIR,
     target_size=(IMG_WIDTH, IMG_HEIGHT),
     batch_size=BATCH_SZ,
-    color_mode='grayscale',
+    color_mode='rgb',
     seed=SEED_RANDOM,
     shuffle=True,
     class_mode='categorical'
@@ -53,35 +58,32 @@ num_classes = train_generator.num_classes
 
 print("\n##\n")
 
+tf_base = applications.densenet.DenseNet201(weights='imagenet',
+                                            include_top=False,
+                                            input_shape=(IMG_WIDTH, IMG_HEIGHT, 3))
+
+for layer in tf_base.layers:
+    layer.trainable = False
+
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(IMG_WIDTH, IMG_HEIGHT, 1)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(tf_base)
 
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.8))
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(40))
-model.add(Activation('softmax'))
+model.add(Dense(40, activation='softmax'))
 
-model.compile(optimizer='adam',
+model.compile('adam',
               loss='categorical_crossentropy',
-              metrics=['accuracy']
-              )
+              metrics=['accuracy'])
 
 model.summary()
 
 print("\n##\n")
 
+early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=3)
 save_callback = callbacks.ModelCheckpoint(MODEL_SAVE_PATH,
                                           monitor='val_acc',
                                           verbose=0,
@@ -99,7 +101,7 @@ history = model.fit_generator(
     epochs=NUM_EPOCHS,
     verbose=2,
     max_queue_size=10000,
-    callbacks=[save_callback]
+    callbacks=[save_callback, early_stopping]
 )
 
 print(f"\n## Treinamento finalizado em {time.perf_counter() - start:.0f}s ##\n")
